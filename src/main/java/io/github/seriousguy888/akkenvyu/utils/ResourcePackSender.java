baaa6115fbf7d9bc292604ed80f82cf7b6df8b74
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2024-03-10 22:37:01 -0400

    fix: handle invalid download urls and hashes

--- src/main/java/io/github/seriousguy888/akkenvyu/utils/ResourcePackSender.java
+++ src/main/java/io/github/seriousguy888/akkenvyu/utils/ResourcePackSender.java
@@ -8,13 +8,18 @@ public class ResourcePackSender {
     public static void sendResourcePack(AkKenVyu plugin, Player player) {
         String dlUrl = plugin.getCachedDataConfig().getDownloadUrl();
 
-        if (dlUrl == null) {
+        if (dlUrl == null || dlUrl.isBlank()) {
             player.sendMessage("Cannot send you the resource pack because"
                     + " the download URL is currently unavailable for some reason.");
             return;
         }
 
         byte[] hash = HexStringToByteArray.convert(plugin.getCachedDataConfig().getHash());
+        if(hash.length != 20) {
+            // The hash must be 20 bytes long, and it will throw an error otherwise.
+            hash = null;
+        }
+
         player.setResourcePack(dlUrl, hash, "Applying latest resource pack...", false);
     }
 }
