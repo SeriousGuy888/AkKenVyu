5fb46252afceebfe695e0c5822f03587798e1bd3
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-02-12 00:41:35 -0500

    add new resource pack status messages
    now possible due to api changes

--- src/main/java/io/github/seriousguy888/akkenvyu/listeners/ResourcePackStatusListener.java
+++ src/main/java/io/github/seriousguy888/akkenvyu/listeners/ResourcePackStatusListener.java
@@ -22,11 +22,14 @@ public class ResourcePackStatusListener implements Listener {
 
         switch (status) {
             case ACCEPTED -> player.sendMessage("Resource pack accepted. Beginning download...");
-            case SUCCESSFULLY_LOADED -> player.sendMessage("Resource pack loaded!");
-            case FAILED_DOWNLOAD ->
-                    player.sendMessage("Resource pack failed to download for some reason. (invalid URL?)");
             case DECLINED -> player.sendMessage("Resource pack declined by client.\n" +
                     "Make sure you have 'Server Resource Packs' set to 'Enabled' or 'Prompt' in the server list.");
+            case DISCARDED -> player.sendMessage("Client has discarded resource pack for some reason.");
+            case DOWNLOADED -> player.sendMessage("Resource pack downloaded.");
+            case FAILED_DOWNLOAD -> player.sendMessage("Resource pack failed to download.");
+            case FAILED_RELOAD -> player.sendMessage("Client failed to reload resource pack.");
+            case INVALID_URL -> player.sendMessage("The resource pack URL sent by the server was invalid.");
+            case SUCCESSFULLY_LOADED -> player.sendMessage("Resource pack loaded successfully!");
             default -> player.sendMessage("Resource pack status: " + status.name());
         }
     }
